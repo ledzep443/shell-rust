b1a1452d531fe6f71e82ddb49edd44f780d307b9
Jimmy Purnell <jimmy@streamingsolutionsusa.com>
2025-09-13 15:15:34 -0400

    Implement exit command

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
+fn main() -> i32 {
     // Uncomment this block to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
 
     // Wait for user input
     let mut input = String::new();
-    while input != "exit" {
+    while input != "exit 0" {
         io::stdin().read_line(&mut input).unwrap();
         println!("{}: command not found", input.trim());
         input.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
     }
+    0
 }
