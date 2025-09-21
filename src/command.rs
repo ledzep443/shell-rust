55982390f80b5487f58f62ceb458a22a913d2d69
Jimmy Purnell <jimmy@streamingsolutionsusa.com>
2025-09-21 16:08:31 -0400

    Implement echo command

---
+++ src/command.rs
@@ -0,0 +1,17 @@
+
+pub(crate) enum Command {
+    Exit,
+    Echo { display_string: String },
+    NotFound
+}
+
+impl Command {
+    pub fn from_input(s: &str) -> Self {
+        let input = s.trim().to_ascii_lowercase();
+        match input.trim().split(' ').collect::<Vec<&str>>().first().unwrap() {
+            &"exit" => Self::Exit,
+            &"echo" => Self::Echo { display_string: input["echo ".len()..].to_string() },
+            _ => Self::NotFound
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
+mod command;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use crate::command::Command;
 
 fn main() {
     // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // Wait for user input
-    let mut input = String::new();
     loop {
-        io::stdin().read_line(&mut input).unwrap();
-        if input.trim() == "exit 0" { 
-            break;
-        }
-        println!("{}: command not found", input.trim());
-        input.clear();
+        let stdin = io::stdin();
         print!("$ ");
         io::stdout().flush().unwrap();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        let cmd = Command::from_input(&input);
+        match cmd {
+            Command::Exit => break,
+            Command::Echo { display_string } => println!("{display_string}"),
+            Command::NotFound => println!("{:}: command not found", input.trim()),
+        }
     }
 }
